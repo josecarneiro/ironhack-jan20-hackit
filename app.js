120e9d200a4ab571907ec8e3dff280c4d06adf17
Sonia Meller <mellersonia@gmail.com>
2020-02-21 12:47:25 +0000

    edited cookie max age

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(
     resave: true,
     saveUninitialized: false,
     cookie: {
-      maxAge: 15 * 24 * 60 * 60 * 1000
+      maxAge: 7 * 24 * 60 * 60 * 1000
     },
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
